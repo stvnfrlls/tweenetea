f5f6e694ceb01d1c8b884acf31d4fb29645bcec2
stvnfrlls <stvnfrlls@gmail.com>
2022-10-13 09:54:19 +0800

    Initial Commit

---
+++ faculty/dashboard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faculty Tools</title>
+</head>
+
+<body>
+    <h1>Faculty Tools</h1>
+    <nav>
+        <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="faculty/scanQR.php">Scan QR Code</a></li>
+            <li><a href="faculty/encodeGrades.php">Encode Grades</a></li>
+            <li><a href="faculty/createReminders.php">Create Reminders</a></li>
+            <li><a href="faculty/createAnnouncement.php">Create Announcement</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
